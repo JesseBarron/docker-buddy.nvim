d80902cdca5d6e6c7cae3bfd8cd8aa8f6220836d
jesseBarron <jessebarrondev@gmail.com>
2023-11-17 23:47:06 -0600

    complete the suite of docker actions

--- lua/docker-buddy/actions.lua
+++ lua/docker-buddy/actions.lua
@@ -1,18 +1,14 @@
-local UI = require('docker-buddy.ui')
-
-local api = vim.api
+local Terminal = require'toggleterm.terminal'.Terminal
 local M = {}
-
-local buf
 
 -- Shape of table
 -- {
 -- "ID"= <container id>
 -- "Names"= <name of the container>
 -- }
-local function getRunningContainters()
+function M.getRunningContainters()
   local containers = {}
-  local containerStringList =  vim.fn.systemlist("docker container ls --format='{{json .}}'") 
+  local containerStringList =  vim.fn.systemlist("docker container ls --format='{{json .}}'")
 
   for _, containerJson in pairs(containerStringList) do
     local containerTable = vim.json.decode(containerJson)
@@ -26,10 +22,60 @@ local function getRunningContainters()
 end
 
 local function findContainer(containerIdentifier)
-  local containers = getRunningContainters()
-
-  P(containers)
+  local containers = M.getRunningContainters()
+  for _,container in pairs(containers) do
+    if container['Names'] == containerIdentifier or container['ID'] == containerIdentifier then
+      return container
+    end
+  end
+  error('Container "'.. containerIdentifier ..'" not found', 1)
 end
 
+local function findContainerAndExecuteCommand(containerIdentifier, operation)
+  local success, result = pcall(findContainer, containerIdentifier)
 
-findContainer()
+  if not success then
+    return print(result)
+  end
+
+  local containerId = result["ID"]
+  local resetResult = vim.fn.system("docker container "..operation.." "..containerId)
+
+  if string.find(resetResult, 'Error') then
+    print(resetResult)
+  else
+    print("Successfully "..operation.." Container: "..containerIdentifier)
+  end
+end
+
+function M.restartContainer(containerIdentifier)
+  findContainerAndExecuteCommand(containerIdentifier, 'restart')
+end
+
+function M.pauseContainer(containerIdentifier)
+  findContainerAndExecuteCommand(containerIdentifier, 'pause')
+end
+
+function M.unpauseContainer(containerIdentifier)
+  findContainerAndExecuteCommand(containerIdentifier, 'unpause')
+end
+
+function M.connectToContainer(containerIdentifier)
+  if not pcall(findContainer, containerIdentifier) then
+    return print("Container '"..containerIdentifier.."' not found")
+  end
+
+  local command = 'docker exec -it '..containerIdentifier..' bash'
+  local term = Terminal:new({
+    cmd=command
+  })
+
+  term:toggle()
+end
+-- M.connectToContainer('3a82c4e92a0e')
+
+
+-- TODO:
+-- - Enhance GUI with buffer keybinds, legends, and user input.
+-- - After that work on updating UI with icons for paused running, ect...
+return M
--- lua/docker-buddy/init.lua
+++ lua/docker-buddy/init.lua
@@ -1,9 +1,5 @@
-
-
-
 -- Create A Few Commads
 vim.api.nvim_create_user_command('DocBudRestart', function(args)
-
   P(args)
 end, {
     nargs = "*"
